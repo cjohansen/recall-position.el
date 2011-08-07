6d5f13dbeeb92fd3f3bb73352b3325a2a9110856
Christian Johansen <christian@gitorious.org>
2011-08-07 20:10:11 +0200

    Initial commit

---
+++ recall-position.el
@@ -0,0 +1,50 @@
+;;; recall-position.el --- 
+
+;; Copyright (C) 2011  Christian Johansen
+
+;; Author: Christian Johansen <christian@cjohansen.no>
+;; Author: August Lilleaas <august@augustl.com>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Start a manual "transaction" for buffer navigation by calling
+;; remember-buffer-pos, restore point and window scroll after looking around
+;; with recall-buffer-pos
+;;
+;; Suggested key-bindings: 
+;;
+;; (global-set-key (kbd "C-c C-r") 'remember-buffer-pos)
+;; (global-set-key (kbd "C-c C-s") 'recall-buffer-pos)
+
+(defvar remembered-buffer-pos nil
+  "Saved buffer position. Used by remember-buffer-pos and
+recall-buffer-pos.")
+
+(defun remember-buffer-pos ()
+  "Remembers the current buffer position and window scroll for
+later retrieval by recall-buffer-pos."
+  (interactive)
+  (setq remembered-buffer-pos `(,(point) ,(window-start))))
+
+(defun recall-buffer-pos ()
+  "Repositions point and window as saved previously by
+remember-buffer-pos"
+  (interactive)
+  (goto-char (car remembered-buffer-pos))
+  (set-window-start (selected-window) (car (cdr remembered-buffer-pos))))
+
+(provide 'recall-position)
+;;; recall-position.el ends here
