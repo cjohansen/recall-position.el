f92db177b2cc03b18b30bd62a93c2dc67189f7be
Christian Johansen <christian@gitorious.org>
2011-08-07 21:25:50 +0200

    Add function to toggle remember/recall so we can do with one key binding

--- recall-position.el
+++ recall-position.el
@@ -22,12 +22,19 @@
 ;;
 ;; Start a manual "transaction" for buffer navigation by calling
 ;; remember-buffer-pos, restore point and window scroll after looking around
-;; with recall-buffer-pos
+;; with recall-buffer-pos. Even better, use toggle-buffer-pos for a one-stop
+;; remember/recall.
 ;;
 ;; Suggested key-bindings: 
 ;;
 ;; (global-set-key (kbd "C-c C-r") 'remember-buffer-pos)
 ;; (global-set-key (kbd "C-c C-s") 'recall-buffer-pos)
+;;
+;; Or:
+;; (global-set-key (kbd "C-c C-s") 'toggle-buffer-pos)
+;;
+;; toggle-buffer-pos will remember the position the first time it's called and
+;; recall it the second time.
 
 (defvar remembered-buffer-pos nil
   "Saved buffer position. Used by remember-buffer-pos and
@@ -45,6 +52,14 @@ remember-buffer-pos"
   (interactive)
   (goto-char (car remembered-buffer-pos))
   (set-window-start (selected-window) (car (cdr remembered-buffer-pos))))
+
+(defun toggle-buffer-pos ()
+  "Cycle Remember or recall buffer position."
+  (interactive)
+  (if (not remembered-buffer-pos)
+      (remember-buffer-pos)
+    (recall-buffer-pos)
+    (setq remembered-buffer-pos nil)))
 
 (provide 'recall-position)
 ;;; recall-position.el ends here
